cc1906f2fc66e3ed46489d56a613bb4361a914ef
Mazahar Shaikh <mazahar.shaikh@bfhl.in>
2021-04-09 17:05:13 +0530

    installed TypeScript for the application and created first index page with greet message

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const IndexPage: NextPage = () => {
+  return (
+    <main>
+      <h1>Welcome to Home page</h1>
+      <p>Here we have xmas gifts</p>
+    </main>
+  );
+};
+export default IndexPage;
