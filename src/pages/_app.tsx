df98a9ea08f15e276cf0b5d794d0b1bfacbfdc88
Mazzzy <shaikhmazahar@gmail.com>
2021-04-10 12:19:54 +0530

    added absolute imports and module path aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '../lib/styles.css';
+import '@/lib/styles.css';
 
 function XmasApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
