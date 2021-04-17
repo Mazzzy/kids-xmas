e16cfa1b93d2b8ca109ef8a4a2151d9d7f8be066
Mazzzy <shaikhmazahar@gmail.com>
2021-04-17 21:39:29 +0530

    added types for products specific data

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,1 +1,27 @@
-export * from './wishlist';
+interface ProductItem {
+  productId: number;
+  quantity: number;
+}
+
+export interface WishItem {
+  id: number;
+  userId: number;
+  date: string;
+  products: ProductItem[];
+}
+
+export interface Product {
+  id: number;
+  image: string;
+  price: number;
+  title: string;
+}
+
+export type ProductLocal = Product & { quantity: number };
+
+export type QuickPreview = {
+  id: number;
+  price: number;
+  image: string;
+  title: string;
+};
